b6255186c47c17f192a69a9d11a3d703335fc8f7
dbirlan <daniel.birlan@gmail.com>
2020-10-30 23:36:03 +0100

    addTodos and about route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/addTodo';
+import About from './components/pages/About';
+import './App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Study File Structure',
         completed: false,
       },
@@ -55,15 +62,39 @@ class App extends Component {
       todos: [...this.state.todos.filter((todo) => todo.id !== id)],
     });
   };
+
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
   render() {
     return (
-      <div className="App">
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is my TodoList app v2.0, made by dbirlan in a fast paced environment</p>
+        </React.Fragment>
+    )
+}
